2ff0a9e8546582612fe07b0ef9c67c56cb9dbc39
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-01 18:52:31 -0400

    Yay readFile() is working and test is coming back all  clear!

--- src/test/java/test/FileServiceTest.java
+++ src/test/java/test/FileServiceTest.java
@@ -15,9 +15,20 @@ class FileServiceTest {
     @Test
     void test_read_file() throws IOException {
         FileService fileService = new FileService();
-        List<Recipe> recipes = new ArrayList<>();
+        List<Recipe> recipes;
 
         recipes = fileService.readFile();
-        assertEquals(recipes.get(1).getGlutenFree(), true);
+        assertEquals(recipes.get(16).getCookingMinutes(), 65);
+        assertEquals(recipes.get(16).getDairyFree(), false);
+        assertEquals(recipes.get(16).getGlutenFree(), false);
+        assertEquals(recipes.get(16).getInstructions(), "Preheat oven to 350° F. Butter a standard loaf pan or a 9\" round or square baking pan.Melt the butter and allow to cool.Meanwhile trim off the zucchini ends and thinly slice 6 or 7 rounds off the end for garnish. Coarsely grate the rest on the large holes of a box grater. Add it to the middle of a clean kitchen towel and gently ring out some (you don't need to get it all out) of the water.Into a large bowl sift both flours, cornmeal, baking powder, salt, and baking soda together.Whisk the eggs and buttermilk into the cooled butter then stir in the zucchini then the cheese.Add the zucchini mixture to the flour mixture and stir until just combined - the batter will be thick.Transfer to the prepared pan and smooth out the top. Arrange the zucchini slices over top.Bake until golden and a tester inserted into center comes out clean, 55-65 minutes for a loaf pan 40 - 50 minutes for a round or square pan.");
+        assertEquals(recipes.get(16).getPreparationMinutes(), 25.0);
+        assertEquals(recipes.get(16).getPricePerServing(), 416.92);
+        assertEquals(recipes.get(16).getReadyInMinutes(), 90);
+        assertEquals(recipes.get(16).getServings(), 1);
+        assertEquals(recipes.get(16).getSpoonacularScore(), 99.0);
+        assertEquals(recipes.get(16).getTitle(), "Cheddar Zucchini Cornbread");
+        assertEquals(recipes.get(16).getVegan(), false);
+        assertEquals(recipes.get(16).getVegetarian(), true);
     }
 }
