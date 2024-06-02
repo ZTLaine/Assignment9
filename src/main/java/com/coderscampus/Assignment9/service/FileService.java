03ba562d2344acaafbc83771b60b2f3fdc9e84fd
Zack Laine <rumtehk_uh@yahoo.com>
2024-06-01 21:30:07 -0400

    Got the streams set up for the @GetMapping, need to look into setting up some tests for the streams in those tomorrow.

--- src/main/java/com/coderscampus/Assignment9/service/FileService.java
+++ src/main/java/com/coderscampus/Assignment9/service/FileService.java
@@ -4,6 +4,7 @@ import com.coderscampus.Assignment9.domain.Recipe;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.csv.QuoteMode;
+import org.springframework.stereotype.Service;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+@Service
 public class FileService {
 
     public List<Recipe> readFile() throws IOException {
@@ -30,24 +32,26 @@ public class FileService {
                 .build()
                 .parse(in);
         for (CSVRecord record : records) {
-            Recipe recipe = new Recipe();
-            recipe.setCookingMinutes(parseField(record, "Cooking Minutes", Integer::parseInt));
-            recipe.setDairyFree(parseField(record, "Dairy Free", Boolean::parseBoolean));
-            recipe.setGlutenFree(parseField(record, "Gluten Free", Boolean::parseBoolean));
-            recipe.setInstructions(parseField(record, "Instructions", Function.identity()));
-            recipe.setPreparationMinutes(parseField(record, "Preparation Minutes", Double::parseDouble));
-            recipe.setPricePerServing(parseField(record, "Price Per Serving", Double::parseDouble));
-            recipe.setReadyInMinutes(parseField(record, "Ready In Minutes", Integer::parseInt));
-            recipe.setServings(parseField(record, "Servings", Integer::parseInt));
-            recipe.setSpoonacularScore(parseField(record, "Spoonacular Score", Double::parseDouble));
-            recipe.setTitle(parseField(record, "Title", Function.identity()));
-            recipe.setVegan(parseField(record, "Vegan", Boolean::parseBoolean));
-            recipe.setVegetarian(parseField(record, "Vegetarian", Boolean::parseBoolean));
-            recipes.add(recipe);
-            System.out.println(recipeCounter + " completed!");
-            System.out.println("~~~~");
-            System.out.println();
-            recipeCounter++;
+            if(record.getRecordNumber() != 1) {
+                Recipe recipe = new Recipe();
+                recipe.setCookingMinutes(parseField(record, "Cooking Minutes", Integer::parseInt));
+                recipe.setDairyFree(parseField(record, "Dairy Free", Boolean::parseBoolean));
+                recipe.setGlutenFree(parseField(record, "Gluten Free", Boolean::parseBoolean));
+                recipe.setInstructions(parseField(record, "Instructions", Function.identity()));
+                recipe.setPreparationMinutes(parseField(record, "Preparation Minutes", Double::parseDouble));
+                recipe.setPricePerServing(parseField(record, "Price Per Serving", Double::parseDouble));
+                recipe.setReadyInMinutes(parseField(record, "Ready In Minutes", Integer::parseInt));
+                recipe.setServings(parseField(record, "Servings", Integer::parseInt));
+                recipe.setSpoonacularScore(parseField(record, "Spoonacular Score", Double::parseDouble));
+                recipe.setTitle(parseField(record, "Title", Function.identity()));
+                recipe.setVegan(parseField(record, "Vegan", Boolean::parseBoolean));
+                recipe.setVegetarian(parseField(record, "Vegetarian", Boolean::parseBoolean));
+                recipes.add(recipe);
+                System.out.println(recipeCounter + " completed!");
+                System.out.println("~~~~");
+                System.out.println();
+                recipeCounter++;
+            }
         }
         return recipes;
     }
--- src/main/java/com/coderscampus/Assignment9/web/RecipeController.java
+++ src/main/java/com/coderscampus/Assignment9/web/RecipeController.java
@@ -1,43 +1,56 @@
 package com.coderscampus.Assignment9.web;
 
 import com.coderscampus.Assignment9.domain.Recipe;
+import com.coderscampus.Assignment9.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class RecipeController {
 
+    @Autowired
+    FileService fileService;
+
     @GetMapping("/gluten-free")
-    public List<Recipe> glutenFree(List<Recipe> recipes) {
-//        Slap the sort logic in here
-        List<Recipe> glutenFreeRecipes = new ArrayList<>();
+    public List<Recipe> glutenFree() throws IOException {
+        List<Recipe> glutenFreeRecipes = fileService.readFile().stream()
+                .filter(Recipe::getGlutenFree)
+                .toList();
         return glutenFreeRecipes;
     }
 
     @GetMapping("/vegan")
-    public List<Recipe> vegan(List<Recipe> recipes) {
-//        Slap the sort logic in here
-        List<Recipe> veganRecipes = new ArrayList<>();
+    public List<Recipe> vegan() throws IOException {
+        List<Recipe> veganRecipes = fileService.readFile().stream()
+                .filter(Recipe::getVegan)
+                .toList();
         return veganRecipes;
     }
 
     @GetMapping("/vegan-and-gluten-free")
-    public List<Recipe> veganAndGlutenFree(List<Recipe> recipes) {
-//        Slap the sort logic in here
-        List<Recipe> veganAndGlutenFreeRecipes = new ArrayList<>();
+    public List<Recipe> veganAndGlutenFree() throws IOException {
+        List<Recipe> veganAndGlutenFreeRecipes = fileService.readFile().stream()
+                .filter(Recipe::getVegan)
+                .filter(Recipe::getGlutenFree)
+                .toList();
         return veganAndGlutenFreeRecipes;
     }
 
     @GetMapping("/vegetarian")
-    public List<Recipe> vegetarian(List<Recipe> recipes) {
-//        Slap the sort logic in here
-        List<Recipe> vegetarianRecipes = new ArrayList<>();
+    public List<Recipe> vegetarian() throws IOException {
+        List<Recipe> vegetarianRecipes = fileService.readFile().stream()
+                .filter(Recipe::getVegetarian)
+                .toList();
         return vegetarianRecipes;
     }
 
     @GetMapping("/all-recipes")
-    public List<Recipe> allRecipes(List<Recipe> recipes) {
-        return recipes;
+    public List<Recipe> allRecipes() throws IOException {
+        return fileService.readFile();
     }
 }
